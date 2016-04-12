9b126c1bc3518a5f860ecdf97d0a2e9e21ac82c2
PetrBarborka <grim.ramone@gmail.com>
2016-04-12 20:52:49 +0200

    Tables tweaked, plots changed into tex boxplots

--- Code/W_dir/pandas_stats.py
+++ Code/W_dir/pandas_stats.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 
 """Create tables & graphs from datasets generated with
 	get_data.py"""
@@ -29,11 +29,16 @@ parser.add_argument('--comboperf', help='show descriptor/descriptor average time
 parser.add_argument('--dettimes', help='show descriptor average time table', action="store_true")
 parser.add_argument('--desctimes', help='show descriptor average time table', action="store_true")
 parser.add_argument('--matchcount', help='avg count of matches/inliers/score', action="store_true")
+
+parser.add_argument('--all_tabs', help='generate all tables (see -h)', action="store_true")
+
 parser.add_argument('--graphzoom', help='graph performance on zoom dataset', action="store_true")
 parser.add_argument('--graphrot', help='graph performance on rot dataset', action="store_true")
 
-parser.add_argument('--save', help='save all drawn plots', action="store_true")
+# parser.add_argument('--save', help='save all drawn plots', action="store_true")
 parser.add_argument('--tex', help='save all drawn plots as tex', action="store_true")
+
+parser.add_argument('--out', help='specify folder to which to put files', type=str)
 
 # --detperf --descperf --comboperf --dettimes --desctimes --matchcount --graphzoom --graphrot
 
@@ -42,6 +47,9 @@ figs = []
 texs = []
 filenames = []
 args = parser.parse_args()
+
+if args.all_tabs:
+	args.detperf = args.descperf = args.comboperf = args.dettimes = args.desctimes = args.matchcount = True
 
 # -------------------------------------------------------------------------------------------------------------
 # functions:
@@ -70,6 +78,17 @@ def plotTable(df, title, header=None):
 	# tab.set_fontsize(50)
 	# plt.show()
 
+def plotGraph(df, title):
+
+	dfplt = df.plot()
+	dfplt.set_title(title, fontsize="x-large")
+
+	fig = dfplt.figure
+
+	figs.append(fig)
+	fignames.append(title)
+
+
 def createMeanTable(datasets, cat, cat_vals, columns):
 	tmplst = []
 	for dm in cat_vals:
@@ -88,7 +107,8 @@ def createComboTable(datasets, cat1, cat2, cat1_vals, cat2_vals, columns):
 	# cat = "det"
 	for dm1 in cat1_vals:
 		for dm2 in cat2_vals:
-			tmplst.append([dm1 + " ->" + dm2])
+			# tmplst.append([dm1 + " ->" + dm2])
+			tmplst.append([dm1, dm2])
 			for ds in datasets:
 				for c in columns:
 					# tmplst[-1].append(ds[ds[cat1] == dm1 and ds[cat2] == dm2].mean()[c])
@@ -100,7 +120,7 @@ def createComboTable(datasets, cat1, cat2, cat1_vals, cat2_vals, columns):
 	df_performance = pd.DataFrame(tmplst)
 	return df_performance
 
-def graphDataset(df_tst, title):
+def graphDataset(df_tst):
 
 	df_tst["methods"] = df_tst["det"] + " ->" + df_tst["desc"]
 	df_tst["pics"] = df_tst["pic1"] + " ->" + df_tst["pic2"]
@@ -111,34 +131,21 @@ def graphDataset(df_tst, title):
 	out = []
 	num = re.compile(ur"\d+")
 	for p in pics:
-		out.append( int(re.findall(num, p)[1]) )
+		out.append( int(re.findall(num, p)[1]))
 	df_tst["pics"] = out
 
 	# print pics
 	# df_tst["pics"] = df_tst["pics"][-4:]
 	df_tst = df_tst.pivot(index="pics", columns="methods", values="score")
 
-	# df_tst["pics"] = df_tst["pics"].split()
+	return df_tst
 
-	# print df_tst.head(100)
 
-	# plotTable(df_tst)
-	# fig = plt.figure()
-	dfplt = df_tst.plot()
-	dfplt.set_title(title, fontsize="x-large")
-
-	fig = dfplt.figure
-
-	figs.append(fig)
-	fignames.append(title)
-
-	# plt.plot(df_tst.values)
-
-	# plt.show()
-
-def createTexTable(values, form=None, header=None, r_header=None, label=None):
+def createTexTable(values, form=None, header=None, r_header=None, label=None, caption=None):
 
 	width = len(values[0])
+
+	outstr = "\\begin{table}[htbp]\\centering\n"
 
 	if r_header is not None:
 		assert len(r_header) == len(values), "row header has to be same length as number of rows in values: len(values) == " \
@@ -146,17 +153,14 @@ def createTexTable(values, form=None, header=None, r_header=None, label=None):
 		width += 1
 
 	if form is None:
-		outstr = "\\begin{tabular}{ " + "l " * width + "}\n"
+		outstr += "\\begin{tabular}{ " + "l " * width + "}\n"
 	else:
 		assert len(form) == len(values[0]), "form has to be same length as number of columns in values + header if present: \
 											len(values[0]) == " \
-											+ width + " len(form) == " + str(len(form))
+											+ str(width) + " len(form) == " + str(len(form))
 		fstr = ""
 		for f in form: fstr += f + " "
-		outstr = "\\begin{tabular}{ " + fstr + "}\n"
-
-	if label is not None:
-		outstr += "\\label{" + label + "}\n"
+		outstr += "\\begin{tabular}{ " + fstr + "}\n"
 
 	if header is not None:
 		assert len(header) == len(values[0]), "header has to be same length as number of columns in values: len(values[0]) == " \
@@ -175,7 +179,7 @@ def createTexTable(values, form=None, header=None, r_header=None, label=None):
 		for h in header:
 			outstr += str(h) + " & "
 		outstr = outstr[:-2]
-		outstr += "\\\\\n\t\hline\n"
+		outstr += "\\\\\n\t\\hline\n"
 
 	r_h_idx = 0
 
@@ -200,10 +204,67 @@ def createTexTable(values, form=None, header=None, r_header=None, label=None):
 		outstr = outstr[:-2]
 		outstr += "\\\\\n"
 	outstr = outstr[:-4]
-	outstr += "\n\end{tabular}"
-
+	outstr += "\n\\end{tabular}\n"
+	if caption is not None:
+		outstr += "	\\caption[Short Heading]{\\protect " + caption + "}"
+		if label is not None:
+			outstr += "\\label{" + label + "}"
+		outstr += "\n"
+	outstr += "\\end{table}"
 	return outstr
 
+def createTexBoxplot(values, span, header=None, label=None, caption=None):
+	tv = np.zeros((len(values[0]), len(values)))
+	# mtrx transposition
+	for i in range(len(values)):
+		for j in range(len(values[0])):
+			tv[j][i] = values[i][j]
+	values = np.array(tv, dtype=float)
+	tv = []
+	for i in range(len(values)):
+		# [mean - std, mean, mean std]
+		m = np.nanmean(values[i])
+		v = np.nanstd(values[i])
+		tv.append([np.clip(m-v, 0, 100), np.clip(m, 0, 100), np.clip(m+v, 0, 100)])
+	values = np.array(tv)
+	outstr = "% Preamble: \pgfplotsset{width=7cm,compat=1.13}\usepgfplotslibrary{statistics}\n" + \
+			"\\begin{figure} \n " + \
+			"\\begin{tikzpicture} \n " + \
+			"\\footnotesize\n " + \
+			"\t\\begin{axis}[ \n" + \
+			"\t\tboxplot/draw direction=y, \n" + \
+			"\t\txticklabel style = {xshift=-0.3cm, rotate=75},\n" + \
+			"\t\tylabel = výkonnost \\%,\n" + \
+			"\t\txtick={"
+	for i in range(len(values)-1):
+		outstr += str(i) + ", "
+	outstr += str(len(values)-1)
+	outstr += "},\n"
+	if header is not None:
+		outstr += "\t\txticklabels={"
+		for h in header:
+			outstr += h + ", "
+		outstr = outstr[:-2] + "}\n"
+	outstr += "\t]\n"
+	c = 0
+	for v in values:
+		outstr += "\t\\addplot+[\n\t\tboxplot prepared={\n\t\tdraw position=" + str(c) + ",\n"
+		outstr += "\t\tlower whisker=" + str(span[0]) + ",\n\t\tlower quartile=" + str(v[0]) + ",\n"
+		outstr += "\t\tmedian=" + str(v[1]) + ",\n\t\tupper quartile=" + str(v[2]) + ",\n"
+		outstr += "\t\tupper whisker=" + str(span[1]) + "}]\n"
+		outstr += "\t\tcoordinates {};\n"
+		# outstr += "val " + str(c) + ":\n" + str(v) + "\n"
+		# outstr += "type of v[0]: " + str(type(v[0])) + "\n"
+		c += 1
+
+	outstr += "\t\\end{axis} \n  \\end{tikzpicture}\n"
+	if caption is not None:
+		outstr += "	\\caption[Short Heading]{\\protect " + caption + "}"
+		if label is not None:
+			outstr += "\\label{" + label + "}"
+		outstr += "\n"
+	outstr += "\\end{figure}"
+	return outstr
 
 # header
 cols = ["folder", "pic1", "pic2", "det", "desc", "matches", "inliers", "score", "det time",
@@ -226,9 +287,9 @@ datasets = [df, df_zoom, df_blur, df_rot, df_angle, df_light, df_res]
 # TABS: ============================================================
 if args.detperf:
 	det_methods = [" Harris", " GFTT", " SIFT", " SURF", " FAST", " MSER", " ORB"]
-	det_header = [	"det method", "total score [%]", "zoom score [%]", "blur score [%]", "rot score [%]",
-				  	"angle score [%]", "light score [%]", "res score [%]"]
-	title = "Detector performance across datasets"
+	det_header = ["Detektor", "celkově[%]", "zoom[%]", "blur[%]", "rot[%]",
+				   "angle[%]", "light[%]", "res[%]"]
+	title = "Celková výkonnost detektorů na datasetech"
 	cat = "det"
 
 	tab = createMeanTable(datasets, cat, det_methods, ["score"])
@@ -237,17 +298,17 @@ if args.detperf:
 	filenames.append(filename)
 
 	if args.tex:
-		texs.append(createTexTable(tab.values, header=det_header, label=filename, form=["l|", "r", "r", "r", "r", "r", "r", "r"]))
+		texs.append(createTexTable(tab.values, header=det_header, label=filename, form=["l|", "r", "r", "r", "r", "r", "r", "r"], caption=title))
 	else:
 		plotTable(tab, title, det_header)
 
 if args.descperf:
 	det_methods = [" BRIEF", " SIFT", " SURF", " ORB"]
-	desr_header = ["desc method", "total score [%]", "zoom score [%]", "blur score [%]", "rot score [%]",
-				  	"angle score [%]", "light score [%]", "res score [%]"]
+	desr_header = ["Deskriptor", "celkově[%]", "zoom[%]", "blur[%]", "rot[%]",
+				   "angle[%]", "light[%]", "res[%]"]
 	cat = "desc"
 
-	title = "Descriptor performance across datasets"
+	title = "Celková výkonnost deskriptorů na datasetech"
 
 	tab = createMeanTable(datasets, cat, det_methods, ["score"])
 
@@ -255,7 +316,7 @@ if args.descperf:
 	filenames.append(filename)
 
 	if args.tex:
-		texs.append(createTexTable(tab.values, header=desr_header, label=filename, form=["l|", "r", "r", "r", "r", "r", "r", "r"]))
+		texs.append(createTexTable(tab.values, header=desr_header, label=filename, form=["l|", "r", "r", "r", "r", "r", "r", "r"], caption=title))
 	else:
 		plotTable(tab, title, desr_header)
 
@@ -264,9 +325,9 @@ if args.comboperf:
 	desc_methods = [" BRIEF", " SIFT", " SURF", " ORB"]
 	# desr_header = [	"methods", "total score [%]", "zoom score [%]", "blur score [%]", "rot score [%]",
 				  	# "angle score [%]", "light score [%]", "res score [%]"]
-	desr_header = [	"methods", "total[%]", "zoom[%]", "blur[%]", "rot[%]",
-				  	"angle[%]", "light[%]", "res[%]"]
-	title = "Detector+descriptor performance across datasets"
+	desr_header = [	"Detektor", "Deskriptor", "celkově[%]", "zoom[%]", "blur[%]", "rot[%]",
+				   "angle[%]", "light[%]", "res[%]"]
+	title = "Celková výkonnost kombinací detektor -> deskriptor"
 
 	tab = createComboTable(datasets, "det", "desc", det_methods, desc_methods, ["score"])
 
@@ -274,16 +335,16 @@ if args.comboperf:
 	filenames.append(filename)
 
 	if args.tex:
-		texs.append(createTexTable(tab.values, header=desr_header, label=filename, form=["l|", "r", "r", "r", "r", "r", "r", "r"]))
+		texs.append(createTexTable(tab.values, header=desr_header, label=filename, form=["l", "l|", "r", "r", "r", "r", "r", "r", "r"], caption=title))
 	else:
 		plotTable(tab, title, desr_header)
 
 if args.dettimes:
 	det_methods = [" Harris", " GFTT", " SIFT", " SURF", " FAST", " MSER", " ORB"]
-	det_header = ["Detection method", "avg det time [s]"]
+	det_header = ["Detektor", "průměrný čas detekce [s]"]
 
 	cat = "det"
-	title = "Detection times across datasets"
+	title = "Průměrné časy detekce"
 
 	columns = ["det time"]
 
@@ -293,16 +354,16 @@ if args.dettimes:
 	filenames.append(filename)
 
 	if args.tex:
-		texs.append(createTexTable(tab.values, header=det_header, label=filename, form=["l|", "r"]))
+		texs.append(createTexTable(tab.values, header=det_header, label=filename, form=["l|", "r"], caption=title))
 	else:
 		plotTable(tab, title, det_header)
 
 if args.desctimes:
 	det_methods = [" BRIEF", " SIFT", " SURF", " ORB"]
-	desr_header = ["Description method", "avg desc time [s]"]
+	desr_header = ["Deskriptor", "průměrný čas deskripce [s]"]
 	# tmplst = []
 	cat = "desc"
-	title = "Description times across datasets"
+	title = "Průměrné časy deskripce"
 
 	columns = ["desc time"]
 
@@ -312,15 +373,15 @@ if args.desctimes:
 	filenames.append(filename)
 
 	if args.tex:
-		texs.append(createTexTable(tab.values, header=desr_header, label=filename, form=["l|", "r"]))
+		texs.append(createTexTable(tab.values, header=desr_header, label=filename, form=["l|", "r"], caption=title))
 	else:
 		plotTable(tab, title, desr_header)
 
 if args.matchcount:
 	det_methods = [" Harris", " GFTT", " SIFT", " SURF", " FAST", " MSER", " ORB"]
 	desc_methods = [" BRIEF", " SIFT", " SURF", " ORB"]
-	header = ["methods", "avg matches", "avg inliers", "avg score [%]"]
-	title = "Match count across datasets"
+	header = ["Detektor", "Deskriptor", "průměrně párů", "průměrně použitých párů", "průměrné skóre [%]"]
+	title = "Počty nalezených párů bodů"
 
 	tab = createComboTable([df], "det", "desc", det_methods, desc_methods, ["matches", "inliers", "score"])
 
@@ -328,7 +389,7 @@ if args.matchcount:
 	filenames.append(filename)
 
 	if args.tex:
-		texs.append(createTexTable(tab.values, header=header, label=filename, form=["l|", "r", "r", "r"]))
+		texs.append(createTexTable(tab.values, header=header, label=filename, form=["l", "l|", "r", "r", "r"], caption=title))
 	else:
 		plotTable(tab, title, header)
 
@@ -336,15 +397,48 @@ if args.matchcount:
 
 if args.graphzoom:
 
-	graphDataset(df_1zoom, "Method combination performance across Asterix dataset (zoom)")
+	det_methods = ["Harris", "GFTT", "SIFT", "SURF", "FAST", "MSER", "ORB"]
+	desc_methods = ["BRIEF", "SIFT", "SURF", "ORB"]
+
+	header = []
+	for dt in det_methods:
+		for dsc in desc_methods:
+			header.append(dt + " -> " + dsc)
+
+	filename = "graph_zoom"
+	filenames.append(filename)
+
+	title = "Střední hodnota a standartní odchylka výkonnosti kombinací metod na datasetu Asterix (zoom)"
+	df = graphDataset(df_1zoom)
+	if args.tex:
+		texs.append(createTexBoxplot(df.values, [0, 100], header=header, caption=title, label=filename))
+	else:
+		plotGraph(df, title)
 
 if args.graphrot:
 
-	graphDataset(df_1rot, "Method combination performance across Monet dataset (rotation)")
+	det_methods = ["Harris", "GFTT", "SIFT", "SURF", "FAST", "MSER", "ORB"]
+	desc_methods = ["BRIEF", "SIFT", "SURF", "ORB"]
 
-if args.save:
+	header = []
+	for dt in det_methods:
+		for dsc in desc_methods:
+			header.append(dt + " -> " + dsc)
+
+	filename = "graph_rot"
+	filenames.append(filename)
+
+	title = "Střední hodnota a standartní odchylka výkonnosti kombinací metod na datasetu Monet (rotace)"
+	df = graphDataset(df_1rot)
+	if args.tex:
+		texs.append(createTexBoxplot(df.values, [0, 100], header=header, caption=title, label=filename))
+	else:
+		plotGraph(df, title)
+
+
+if args.out is not None:
 	for i in range(len(filenames)):
-		finame = filenames[i]
+		finame = args.out + filenames[i]
 		if args.tex:
 			finame += ".tex"
 			with open(finame, "w") as fileToWrite:
